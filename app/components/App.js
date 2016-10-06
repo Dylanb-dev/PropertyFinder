03f6fa15260b1687ad939a8e5988f073a7f53971
lolcookie <dylan.broadbridge@gmail.com>
2016-10-06 22:25:25 +0800

    added Searchpage and routing

--- app/components/App.js
+++ app/components/App.js
@@ -4,54 +4,94 @@
  * Follow me https://twitter.com/grabthecode
  */
 
+
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
+ AppRegistry,
+ StyleSheet,
+ Text,
+ View,
+ TouchableHighlight,
 } from 'react-native';
 
-import { Touchable } from './Touchable';
+import { Match, Miss, MemoryRouter as Router } from 'react-router';
+
+import { SearchPage } from './SearchPage.js'
+
 
 export class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello World (Again)</Text>
-      </View>
-    );
-  }
+ render() {
+   return (
+     <Router>
+       <View style={styles.container}>
+         <View style={styles.routeContainer}>
+           <Match exactly pattern="/" component={componentFactory('Home')}/>
+           <Match pattern="/searchpage" component={ SearchPage }/>
+           <Match pattern="/topics" component={componentFactory('Topics')}/>
+           <Miss component={componentFactory('Nope, nothing here')}/>
+         </View>
+         <View style={styles.container}>
+           <NavLink to="/">
+             <Text style={styles.routeLink}>
+               Home
+             </Text>
+           </NavLink>
+           <NavLink to="/searchpage">
+             <Text style={styles.routeLink}>
+                Search Page
+             </Text>
+           </NavLink>
+           <NavLink to="/topics">
+             <Text style={styles.routeLink}>
+               Topics
+             </Text>
+           </NavLink>
+           <NavLink to="/broken">
+             <Text style={styles.routeLink}>
+               Broken
+             </Text>
+           </NavLink>
+         </View>
+       </View>
+     </Router>
+   );
+ }
 }
+const NavLink = ({to, children}, context) => {
+ const pressHandler = () => context.router.transitionTo(to);
+ return (
+   <TouchableHighlight onPress={pressHandler}>
+     {children}
+   </TouchableHighlight>
+ )
+}
+NavLink.contextTypes = {router: React.PropTypes.object}
+
+// Just a helper function to generate stateless components
+const componentFactory = (routeName) => () => (
+ <View>
+   <Text style={styles.route}>{routeName}</Text>
+ </View>
+)
+
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
-    margin: 80
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  logo: {
-    alignSelf: 'center',
-    marginBottom: 10,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  touchable: {
-    backgroundColor: '#CAE6FE',
-  },
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: '#F5FCFF',
+ },
+ route: {
+   color: '#701010',
+   fontSize: 40
+ },
+ routeLink: {
+   color: '#0000FF'
+ },
+ routeContainer: {
+   flex: 1,
+   justifyContent: 'center'
+ }
+
 });
